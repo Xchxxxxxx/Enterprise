6e8558bf1c80f36251420bf3bb080cdbec17a227
Xchxxxxxx <2932441131@qq.com>
2026-07-15 20:53:01 +0800

    fix: MediatorDomainEventBus Injectable to Scoped

--- src/03-Infrastructure/Services/MediatorDomainEventBus.cs
+++ src/03-Infrastructure/Services/MediatorDomainEventBus.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 
 namespace EfCore.Enterprise.Infrastructure.Services;
 
-[Injectable(ServiceLifetime.Singleton, ExposeAs = typeof(IDomainEventBus))]
+[Injectable(ServiceLifetime.Scoped, ExposeAs = typeof(IDomainEventBus))]
 public class MediatorDomainEventBus : IDomainEventBus
 {
     private readonly IMediator _mediator;
